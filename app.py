71c52b535a8547586262c12a34bca94c6d90d61c
jeonjunwoo0328 <a22640210@gmail.com>
2026-07-20 13:51:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ with tab2:
          st.write("포인트 아이템:")
          acc = st.multiselect("액세서리추가", ["모자", "안경", "목걸이", "가방"])
          with st.expander("액세서리 스타일링 팁 보기"):
-             st.warning("너무 많은 액세서리는 투머치가 될 수 있어요.')
+             st.warning("너무 많은 액세서리는 투머치가 될 수 있어요.")
 
      st.markdown("---")
      if st.button("코디 완성하기"):
