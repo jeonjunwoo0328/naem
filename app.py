31235fe9b744951fd925b65ad1d220d1a59a15b3
jeonjunwoo0328 <a22640210@gmail.com>
2026-07-21 09:32:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ for i in range(len(st.session_state.todo_list)):
 st.markdown("---")
 
 def page3():
-st.header("📈 3. 나의 갓생 지수")
+ st.header("📈 3. 나의 갓생 지수")
 if not st.session_state.todo_list:
     st.write("아직 등록된 할 일이 없습니다.")
 else:
