49fe3e410e209c95484e5aaa4e39313b0f05a2a9
jeonjunwoo0328 <a22640210@gmail.com>
2026-07-21 13:23:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -42,9 +42,8 @@ def create_quiz(field, level):
 if st.button("문제 만들기"):
  with st.spinner("문제를 만드는 중..."): 
   quiz = make_quiz(field,level)
-
-st.success("문제 생성 완료")
-st.markdown(quiz)
+ st.success("문제 생성 완료")
+ st.markdown(quiz)
 
 
 
